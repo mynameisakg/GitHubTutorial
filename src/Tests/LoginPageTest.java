8e86dc3441c74435736744dea3746cd4d9440408
mynameisakg <ankushergupta@gmail.com>
2022-11-20 13:03:02 +0530

    added/updated/deleted

--- src/Tests/LoginPageTest.java
+++ src/Tests/LoginPageTest.java
@@ -1,5 +1,13 @@
 package Tests;
 
 public class LoginPageTest {
+	
+	public void test() {
+		
+		System.out.println("test 1---LP");
+		String un = "admin";
+		String pass = "admin";
+		System.out.println("Logged in user is: "+ un);
+	}
 
 }
